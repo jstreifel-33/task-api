632100cdcd297ef440dac38046c4b37c2341afc9
Joe Streifel <josephstreifel@gmail.com>
2022-01-12 21:06:51 -0800

    updates urls and adds serializer'

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('create/<int:pk>', ),
-    path('update/<int:pk>', ),
+    path('create/<int:pk>', TaskList.as_view(), name='create_task'),
+    path('change/<int:pk>', TaskChange.as_view(), name='change_task'),
 ]
